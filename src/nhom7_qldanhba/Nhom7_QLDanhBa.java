443e44d47a71453d69fc180872c3e9f36a1418db
KhaiTapCode04 <2251120162@ut.edu.vn>
2024-11-19 19:10:43 +0700

    second commit

--- src/nhom7_qldanhba/Nhom7_QLDanhBa.java
+++ src/nhom7_qldanhba/Nhom7_QLDanhBa.java
@@ -4,7 +4,7 @@ package nhom7_qldanhba;
 public class Nhom7_QLDanhBa {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Chao mung toi nhom 7");
     }
     
 }
